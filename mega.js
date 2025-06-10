6e59a49d7a5423239115cc30531f88ad044f2dee
ali-raza-ah <queensapna69@gmail.com>
2025-06-10 19:19:39 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'mhasnainbhatti828@gmail.com',
-    password: 'husnain1',
+    email: 'robers66158@x8oq.awesome47.com',
+    password: 'megaAinz1@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
